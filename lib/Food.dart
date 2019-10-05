73dde81781951fa67f2e058391717bdc1ccd78ad
curtylin <curtylin@gmail.com>
2019-10-05 15:04:37 -0600

    Added a food class to get the ball rolling and changed the app color and name to Masago

---
+++ lib/Food.dart
@@ -0,0 +1,18 @@
+class Food {
+  String image;     //PLEASE CHANGE THIS LATER DOWN THE LINE... IMAGES ARENT STRINGS.. I REPEAT.. IMAGES ARENT STRINGS
+  String restaurantName;
+  int numStars;
+  double price;
+
+  Food()
+  {
+
+  }
+
+
+
+
+
+
+
+}
