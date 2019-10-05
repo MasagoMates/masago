2dc929856083fec9b710707d116596c6a44bd9b0
curtylin <curtylin@gmail.com>
2019-10-05 14:21:56 -0600

    just changed the name from Flutter Demo to Masago

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Masago',
       theme: ThemeData(
         // This is the theme of your application.
         //
